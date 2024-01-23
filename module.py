1b1af5c3041d8239138c2667ca6527fc3b2b4ee0
Arturo Suárez <manuel.suarez@cimat.mx>
2024-01-23 13:07:03 -0600

    Testing

--- module.py
+++ module.py
@@ -24,6 +24,7 @@ class OilSpillModule(LightningModule):
         return self.model(image)
 
     def training_step(self, batch, batch_idx):
+        print(batch, batch_idx)
         image, label = batch
         bs = image.shape[0]
         h, w = image.shape[2:]
