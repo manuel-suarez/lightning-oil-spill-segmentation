23ca8fc5abc84493ca68b17a59f2ca6093a7632b
Arturo Suárez <manuel.suarez@cimat.mx>
2024-01-24 09:43:28 -0600

    Change command line arguments

--- predict.py
+++ predict.py
@@ -1,3 +1,4 @@
+import os
 import argparse
 import numpy as np
 from PIL import Image
@@ -10,21 +11,27 @@ parser = argparse.ArgumentParser(
     epilog='With a great power comes a great responsability'
 )
 parser.add_argument('checkpoint')
-parser.add_argument('normfilename')
-parser.add_argument('varfilename')
+parser.add_argument('basedir')
+parser.add_argument('keyfile')
 args = parser.parse_args()
+keyfile = args.keyfile
+img_dir = os.path.join(args.basedir, keyfile)
+print(img_dir)
 
 # Load checkpoint
 model = OilSpillModule.load_from_checkpoint(args.checkpoint)
 model.eval()
 
 # Open image file
-normimage = np.array(Image.open(args.normfilename))
+filename = os.path.join(img_dir, f"{keyfile}_norm.tif")
+normimage = np.array(Image.open(filename))
+
+print(filename, normimage.shape)
+# Open variance file
+filename = os.path.join(img_dir, f"{keyfile}_var.tif")
 varimage = np.array(Image.open(args.varfilename))
 
-print(normimage.shape, varimage.shape)
-# Open variance file
-
+print(filename, varimage.shape)
 # Compose multichannel image
 
 # Predict
