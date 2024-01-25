d7380371b6318c8cabb73114cf8ff6525b00f77f
Arturo Suárez <manuel.suarez@cimat.mx>
2024-01-24 19:05:47 -0600

    Fix missing argument

--- predict.py
+++ predict.py
@@ -41,5 +41,5 @@ print(predictions.shape)
 result = rearrange(predictions, 'i j c h w -> (i h) (j w) c')
 print(result.shape, np.max(result), np.min(result))
 
-imsave(f"{args.imagekey}_result.png")
+imsave(f"{args.imagekey}_result.png", result)
 print("Done!")
